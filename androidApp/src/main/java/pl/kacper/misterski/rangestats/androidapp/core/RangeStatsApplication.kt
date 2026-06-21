dd2a3b233059ae7d89cefabc0400eb10f041c367
KCPR13 <kacper.misterski13@gmail.com>
2026-06-21 20:15:05 +0200

    code cleanup

--- androidApp/src/main/java/pl/kacper/misterski/rangestats/androidapp/core/RangeStatsApplication.kt
+++ androidApp/src/main/java/pl/kacper/misterski/rangestats/androidapp/core/RangeStatsApplication.kt
@@ -5,11 +5,6 @@ import android.os.Build
 import org.koin.android.ext.koin.androidContext
 import pl.kacper.misterski.rangestats.di.initializeKoin
 
-//1.  screenOrientation nie działa na dużych ekranach
-//2. static final przez reflection
-//3. Charles / nasłuchiwanie proxy przestaje działąć w network_security_config trzeba dodać <certificateTransparency enabled="false">
-//4. Klawiatura po rotacji się ukrywa, fix to windowSoftInputMode="stateAlwaysVisible"
-//5. recreate Activity się zmienia, do testów
 class RangeStatsApplication: Application() {
 
     override fun onCreate() {
@@ -17,12 +12,5 @@ class RangeStatsApplication: Application() {
         initializeKoin {
             androidContext(this@RangeStatsApplication)
         }
-        //reflection()
-    }
-
-    private fun reflection(){ // public static final
-        val field = Build.VERSION::class.java.getDeclaredField("SDK_INT")
-        field.isAccessible = true
-        field.set(null, 99)
     }
 }
