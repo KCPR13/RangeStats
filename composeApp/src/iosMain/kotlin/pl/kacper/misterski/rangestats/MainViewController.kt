7f840a05b2468054a97d6bf6b8126f1561ca1921
KCPR13 <kacper.misterski13@gmail.com>
2026-08-30 16:49:52 +0200

    composeApp cleanup

--- composeApp/src/iosMain/kotlin/pl/kacper/misterski/rangestats/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/pl/kacper/misterski/rangestats/MainViewController.kt
@@ -3,4 +3,5 @@ package pl.kacper.misterski.rangestats
 import androidx.compose.ui.window.ComposeUIViewController
 import pl.kacper.misterski.rangestats.core.App
 
+@Suppress("FunctionNaming", "ktlint:standard:function-naming")
 fun MainViewController() = ComposeUIViewController { App() }
